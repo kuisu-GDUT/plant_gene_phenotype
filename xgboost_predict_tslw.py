dcdeeddad9720636846544b096d4310c26dfa291
kuisu <sukui@genomics.cn>
2024-05-16 16:44:16 +0800

    tslw最佳的结果：0.3294361858554362

--- xgboost_predict_tslw.py
+++ xgboost_predict_tslw.py
@@ -168,45 +168,61 @@ class ML_Model:
         print("Best Parameters: ", grid_search.best_params_)
         best_model = grid_search.best_estimator_
         for i in range(5):
-            best_model.fit(X,Y)
+            best_model.fit(X, Y)
         return best_model
 
-    def xgboost(self, x_train, y_train, x_test=None, y_test=None) -> BaseEstimator:
+    def xgboost(self, x_train, y_train, x_val=None, y_val=None) -> BaseEstimator:
         logging.info("xgboost model train...")
         param_grid = {
-            "learning_rate": [0.1],
-            "n_estimators": [100, 500],
-            # "max_depth": [3, 8],
+            # "learning_rate": [0.1],
+            # "n_estimators": [500],
+            # "max_depth": [9, 10,11],
             # "min_child_weight": [2],
             # "gamma": [0.4],
             # "colsample_bytree": [0.7],
-            "objective": ["reg:squarederror"],
-            "reg_alpha": [0, 0.1, 0.5],
-            "reg_lambda": [0.5, 1, 1.5]
+            # "objective": ["reg:squarederror"],
+            # "reg_alpha": [0.55, 0.6, 0.65],
+            # "reg_lambda": [0.5, 1, 1.5]
         }
+
         model = XGBRegressor(
             learning_rate=0.1,
-            n_estimators=200,  # 树的个数--100棵树建立xgboost
+            n_estimators=150,  # 树的个数--100棵树建立xgboost
             max_depth=12,  # 树的深度
             min_child_weight=2,  # 叶子节点最小权重
             gamma=0.4,  # 惩罚项中叶子结点个数前的参数
             subsample=0.7,  # 随机选择70%样本建立决策树
             colsample_bytree=0.7,  # 随机选择70%特征建立决策树
             objective='reg:squarederror',  # 使用平方误差作为损失函数
-            # reg_alpha=2,
-            # reg_lambda=2,
+            reg_alpha=0.6,
+            reg_lambda=1.5,
         )
         best_model = self.gridsearchcv(
             model=model,
-            param_grid={},
+            param_grid=param_grid,
             X=x_train,
             Y=y_train,
             cv=5
         )
+        if x_val is not None and y_val is not None:
+            best_model.fit(
+                x_train,
+                y_train,
+                eval_set=[(x_val, y_val), (x_train, y_train)],
+                eval_metric=["rmse"],
+                early_stopping_rounds=10,
+                verbose=False
+            )
+            results = best_model.evals_result()
+            y_dict = {
+                "Val": results['validation_0']['rmse'],
+                "Train": results['validation_1']['rmse']
+            }
+            show_fig(y_dict, y_label="RMSE", title="XGBOOST")
 
         return best_model
 
-    def linear_model(self, x_train, y_train, x_test=None, y_test=None) -> BaseEstimator:
+    def linear_model(self, x_train, y_train, x_val=None, y_val=None) -> BaseEstimator:
         logging.info("linear model train...")
         model = linear_model.LinearRegression()
 
@@ -218,7 +234,7 @@ class ML_Model:
 
         return best_model
 
-    def elasticnet(self, x_train, y_train, x_test=None, y_test=None) -> BaseEstimator:
+    def elasticnet(self, x_train, y_train, x_val=None, y_val=None) -> BaseEstimator:
         logging.info("elasticnet model train...")
         model = linear_model.ElasticNet()
         param_grid = {
@@ -243,9 +259,8 @@ class Trainer:
 
     def train(self, df_X, df_Y, fill_nan=False):
         logging.info(f"train shape: {df_X.shape}, label shape: {df_Y.shape}")
-        if fill_nan:
-            df_X = df_X.fillna(-1)
-
+        # if fill_nan:
+        #     df_X = df_X.fillna(-1)
         x_train, x_test, y_train, y_test = train_test_split(
             np.asarray(df_X),
             np.asarray(df_Y),
@@ -253,7 +268,7 @@ class Trainer:
             random_state=33
         )
 
-        model = self.ml.xgboost(x_train=x_train, y_train=y_train)
+        model = self.ml.xgboost(x_train=x_train, y_train=y_train, x_val=x_test, y_val=y_test)
         predict = model.predict(x_test)
         self.eval(predict, label=y_test)
 
@@ -306,7 +321,7 @@ def main():
     assert os.path.exists(save_path), f"{save_path} is not exits."
     task = "TSLW"
     select_feature = "f-value"  # pvalue, f-value
-    max_features_num = 1024
+    max_features_num = 2048
     logging.info(f"task:{task}\nselect_feature:{select_feature}\nmax_features_num:{max_features_num}")
 
     dp = DataProcess()
@@ -318,18 +333,19 @@ def main():
     if os.path.exists(task_snp_path):
         task_snp_df = dp.read_data(task_snp_path, header=0)
         new_names = [i for i in task_snp_df.columns.values if i not in df_merge.columns.values]
-        df_merge = pd.merge(df_merge, task_snp_df[new_names], how="left", left_index=True, right_index=True)
-        logging.info(f"merge shape: {df_merge.shape}, merge snp data: {task_snp_path}")
+        df_feature = pd.merge(df_merge, task_snp_df[new_names], how="left", left_index=True, right_index=True)
+        logging.info(f"merge shape: {df_feature.shape}, merge snp data: {task_snp_path}")
 
     df_merge.sample(frac=1).reset_index(drop=True)
+    df_feature = df_merge.iloc[:, 12:]
     label = df_merge[task]
 
-    if select_feature == "p-vlaue":
-        X_snp_names = [name for name in df_merge.columns.values if "snp" in name]
-        X_otu_names = [name for name in df_merge.columns.values if "OTU" in name]
-        X_snp_pvalue = dp.f_regression(data=df_merge[X_snp_names], label=label)
-        X_otu_pvalue1 = dp.linear_regression(data=df_merge[X_otu_names[:500]], label=label)
-        X_otu_pvalue2 = dp.linear_regression(data=df_merge[X_otu_names[500:]], label=label)
+    if select_feature == "p-value":
+        X_snp_names = [name for name in df_feature.columns.values if "snp" in name]
+        X_otu_names = [name for name in df_feature.columns.values if "OTU" in name]
+        X_snp_pvalue = dp.f_regression(data=df_feature[X_snp_names], label=label)
+        X_otu_pvalue1 = dp.linear_regression(data=df_feature[X_otu_names[:500]], label=label)
+        X_otu_pvalue2 = dp.linear_regression(data=df_feature[X_otu_names[500:]], label=label)
         pvalue_dict = {
             "name": X_snp_names + X_otu_names,
             "pvalue": X_snp_pvalue + X_otu_pvalue1 + X_otu_pvalue2
@@ -341,13 +357,13 @@ def main():
         pvalue_df.to_csv(os.path.join(save_path, "pvalue_order_{}.csv".format(task)))
         select_feature_names = list(r_pvalue["name"].values)[:max_features_num]
     else:
-        sel = SelectKBest(score_func=f_regression, k=min(max_features_num, df_merge.shape[-1]))
-        sel = sel.fit(df_merge.fillna(-1), label)
-        select_feature_names = df_merge.columns[sel.get_support(True)]
+        sel = SelectKBest(score_func=f_regression, k=min(max_features_num, df_feature.shape[-1]))
+        sel = sel.fit(df_feature.fillna(-1), label)
+        select_feature_names = df_feature.columns[sel.get_support(True)]
 
     logging.info(f"select feature nums: {len(select_feature_names)}")
 
-    X = df_merge[select_feature_names]
+    X = df_feature[select_feature_names]
     trainer = Trainer()
     trainer.train(df_X=X, df_Y=label)
 
